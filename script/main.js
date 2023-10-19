766d91c248efa6eb252ea78ecc1ef5f747557299
Farid <makflay85zet@gmail.com>
2023-10-20 02:56:53 +0400

    change main.js added mescount and current Time and messages updated

--- script/main.js
+++ script/main.js
@@ -1,5 +1,17 @@
-function updateText() {
-    var inputText = document.getElementById("textInput").value;
+let messageCount = 1;
 
-    document.getElementById("textDisplay").value = inputText;
+function updateText() {
+    const inputText = document.getElementById("textInput").value;
+
+    const currentTime = new Date();
+    const formattedTime = currentTime.toLocaleString();
+
+    const message = `${messageCount}. [${formattedTime}]: ${inputText}`;
+
+    const textDisplay = document.getElementById("textDisplay");
+    textDisplay.value += message + '\n';
+
+    messageCount++;
+
+    document.getElementById("textInput").value = "";
 }
